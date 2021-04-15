7f9f87a6e305bedf2556f56679e6a04cebcba9f1
Kamila Kowalczyk-Radomska <kamila_kodilla@o2.pl>
2021-04-15 19:29:17 +0200

    Subpage support and repair file change

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,42 @@
-import { settings, select } from './settings.js';
+import { settings, select, classNames } from './settings.js';
 import Product from './components/Product.js';
 import Cart from './components/Cart.js';
 
 const app = {
+
+  initPages: function(){
+    const thisApp = this;
+    
+    thisApp.pages = document.querySelector(select.containerOf.pages).children;
+    thisApp.navLinks = document.querySelectorAll(select.nav.links);
+
+    thisApp.activatePage(thisApp.pages[0].id);
+
+  },
+
+  activatePage: function(pageId){
+    const thisApp = this;
+
+    /* add class"active" to matching pages, remove from non-matching */
+    for(let page of thisApp.pages){
+    //  if(page.id == pageId){
+    //    page.classList.add(classNames.pages.active);
+    //  } else {
+    //    page.classList.remove(classNames.pages.active);
+    //  }
+    page.classList.toggle(classNames.pages.active, page.id == pageId);
+  }
+  
+    for(let link of thisApp.navLinks){
+      link.classList.toggle(
+        classNames.nav.active, 
+        link.getAttribute('href') == '#' + pageId
+        );
+  }
+
+    /* add class"active" to matching pages, remove from non-matching */
+
+  },
 
   initMenu: function () {
     const thisApp = this;
@@ -34,6 +68,7 @@ const app = {
 
   init: function () {
     const thisApp = this;
+    thisApp.initPages();
 
     thisApp.initData();
     thisApp.initCart();
@@ -47,8 +82,7 @@ const app = {
     thisApp.productList = document.querySelector(select.containerOf.menu);
     thisApp.productList.addEventListener('add-to-cart', {
       function(event) {
-        app.cart.add(event.detail.product.prepareCartProduct());
-        console.log(event);
+        app.cart.add(event.detail.product);
       }
     });
   },
